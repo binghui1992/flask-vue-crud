7600b32a45606ec4e3a7e561147b27bb8865c546
kaer.lbh <kaer.lbh@xuelangyun.com>
2023-04-27 17:36:25 +0800

    feat(frontend): add PUT for "/books/<book_id>" for updating an existing book General guideline: 1. Add handler function "toggleEditBookModal(book)" for Update button; 2. In toggleEditBookModal(), open up a new window (done by body.classList.add where body is obtained by document.querySelector) for updating book, well you'll also need to close the window when click the close button on the newly opened window. Moreover, clear the alert message when such window pops up. 3. on update book window, there are 3 buttons: Submit/Cancel/Close, add handler functions for each of these buttons, respectively are: handleEditSubmit()/ handleEditCancel()/toggleEditBookModal(); 4. in handleEditSubmit(), update the book by calling updateBook(payload, bookID) in which makes the call to the updating book API;

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -35,7 +35,12 @@
               </td>
               <td>
                 <div class="btn-group" role="group">
-                  <button type="button" class="btn btn-warning btn-sm">Update</button>
+                  <button
+                    type="button"
+                    class="btn btn-warning btn-sm"
+                    @click="toggleEditBookModal(book)">
+                    Update
+                  </button>
                   <button type="button" class="btn btn-danger btn-sm">Delete</button>
                 </div>
               </td>
@@ -115,10 +120,80 @@
       </div>
     </div>
     <div v-if="activeAddBookModal" class="modal-backdrop fade show"></div>
+
+    <!-- edit book modal -->
+    <div
+      ref="editBookModal"
+      class="modal fade"
+      :class="{ show: activeEditBookModal, 'd-block': activeEditBookModal }"
+      tabindex="-1"
+      role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Update</h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+              @click="toggleEditBookModal">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="mb-3">
+                <label for="editBookTitle" class="form-label">Title:</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="editBookTitle"
+                  v-model="editBookForm.title"
+                  placeholder="Enter title">
+              </div>
+              <div class="mb-3">
+                <label for="editBookAuthor" class="form-label">Author:</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="editBookAuthor"
+                  v-model="editBookForm.author"
+                  placeholder="Enter author">
+              </div>
+              <div class="mb-3 form-check">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="editBookRead"
+                  v-model="editBookForm.read">
+                <label class="form-check-label" for="editBookRead">Read?</label>
+              </div>
+              <div class="btn-group" role="group">
+                <button
+                  type="button"
+                  class="btn btn-primary btn-sm"
+                  @click="handleEditSubmit">
+                  Submit
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-danger btn-sm"
+                  @click="handleEditCancel">
+                  Cancel
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-if="activeEditBookModal" class="modal-backdrop fade show"></div>
   </div>
 </template>
 
 <script>
+// import modules or custom defined components
 import axios from 'axios';
 import Alert from './Alert.vue';
 
@@ -134,6 +209,13 @@ export default {
       books: [],
       message: '',
       showMessage: false,
+      activeEditBookModal: false,
+      editBookForm: {
+        book_id: '',
+        title: '',
+        author: '',
+        read: [],
+      },
     };
   },
   components: {
@@ -150,7 +232,6 @@ export default {
           this.showMessage = true;
         })
         .catch((error) => {
-
           console.log(error);
           this.getBooks();
           this.message = error;
@@ -164,8 +245,20 @@ export default {
           this.books = res.data.books;
         })
         .catch((error) => {
-
           console.error(error);
+        });
+    },
+    updateBook(payload, bookID) {
+      const path = `http://localhost:5001/books/${bookID}`;
+      axios.put(path, payload)
+        .then(() => {
+          this.getBooks();
+          this.message = 'Book updated';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          console.error(error);
+          this.getBooks();
         });
     },
     handleAddReset() {
@@ -189,6 +282,10 @@ export default {
       this.addBookForm.title = '';
       this.addBookForm.author = '';
       this.addBookForm.read = [];
+      this.editBookForm.book_id = '';
+      this.editBookForm.title = '';
+      this.editBookForm.author = '';
+      this.editBookForm.read = [];
     },
     toggleAddBookModal() {
       const body = document.querySelector('body');
@@ -199,6 +296,35 @@ export default {
       } else {
         body.classList.remove('modal-open');  // close the adding book popup window
       }
+    },
+    toggleEditBookModal(book) {
+      if (book) {
+        this.editBookForm = book;
+      }
+      const body = document.querySelector('body');
+      this.activeEditBookModal = !this.activeEditBookModal;
+      this.showMessage = false;
+      if (this.activeEditBookModal) {
+        body.classList.add('modal-open');
+      } else {
+        body.classList.remove('modal-open');
+      }
+    },
+    handleEditSubmit() {
+      this.toggleEditBookModal(null);
+      let read = false;
+      if (this.editBookForm.read) read = true;
+      const payload = {
+        title: this.editBookForm.title,
+        author: this.editBookForm.author,
+        read,
+      };
+      this.updateBook(payload, this.editBookForm.book_id);
+    },
+    handleEditCancel() {
+      this.toggleEditBookModal(null);
+      this.initForm();
+      this.getBooks();  // why?
     },
   },
   created() {
