b48423cbed144de2064564fe962b968b1554cdf8
kaer.lbh <kaer.lbh@xuelangyun.com>
2023-05-04 22:37:34 +0800

    feat(backend): replace memory database (Python objects) with a real database (PostgreSQL)

--- server/app.py
+++ server/app.py
@@ -1,34 +1,43 @@
-import uuid
+import json
+import os
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UUID
+import sqlalchemy
 
 
-BOOKS = [
-    {
-        'book_id': uuid.uuid4().hex,
-        'title': 'On the Road',
-        'author': 'Jack Kerouac',
-        'read': True
-    },
-    {
-        'book_id': uuid.uuid4().hex,
-        'title': 'Harry Potter and the Philosopher\'s Stone',
-        'author': 'J. K. Rowling',
-        'read': False
-    },
-    {
-        'book_id': uuid.uuid4().hex,
-        'title': 'Green Eggs and Ham',
-        'author': 'Dr. Seuss',
-        'read': True
-    }
-]
+config_file = os.path.join(os.path.dirname(__file__), 'database/config.json')
+with open(config_file, "r") as f:
+    DB_CONFIG = json.load(f)
 
 
 # instantiate the app
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f'postgresql://{DB_CONFIG["username"]}:{DB_CONFIG["password"]}@'
+    f'{DB_CONFIG["hostname"]}:{DB_CONFIG["port"]}/{DB_CONFIG["dbName"]}')
+
+
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    __tablename__ = "book"
+    book_id = db.Column(UUID(as_uuid=True), primary_key=True,
+                        server_default=sqlalchemy.text("uuid_generate_v4()"))
+    title = db.Column(db.String(128))
+    author = db.Column(db.String(128))
+    read = db.Column(db.Boolean)
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+with app.app_context():
+    db.create_all()
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
@@ -45,17 +54,16 @@ def all_books():
     response_object = {'status': 'error'}
     if request.method == 'POST':
         post_data = request.get_json()
-        book_id = uuid.uuid4().hex
-        BOOKS.append({
-            'book_id': uuid.uuid4().hex,
-            'title': post_data.get('title'),
-            'author': post_data.get('author'),
-            'read': post_data.get('read'),
-        })
+        book = Book(title=post_data.get('title'),
+                    author=post_data.get('author'),
+                    read=post_data.get('read'))
+        db.session.add(book)
+        db.session.commit()
         response_object = {'status': 'success', 'message': 'Book added!',
-                           'bookId': book_id}
+                           'book_id': book.book_id}
     elif request.method == 'GET':
-        response_object = {'status': 'success', 'books': BOOKS}
+        books = [book.as_dict() for book in db.session.query(Book).all()]
+        response_object = {'status': 'success', 'books': books}
 
     return jsonify(response_object)
 
@@ -63,34 +71,25 @@ def all_books():
 @app.route('/books/<book_id>', methods=['PUT', 'DELETE'])
 def single_book(book_id):
     response_object = {'status': 'error'}
-    if request.method == 'PUT':
-        post_data = request.get_json()
-        incoming_book = {
-            'book_id': book_id,
-            'title': post_data.get('title'),
-            'author': post_data.get('author'),
-            'read': post_data.get('read'),
-        }
 
-        for i, book in enumerate(BOOKS):
-            if book['book_id'] == book_id:
-                BOOKS[i].update(incoming_book)
-                response_object = {'status': 'success',
-                                   'message': 'Book updated!'}
-                break
-        else:
-            response_object = {'status': 'error',
-                               'message': f'Book ID({book_id}) not found!'}
-    elif request.method == 'DELETE':
-        for i, book in enumerate(BOOKS):
-            if book['book_id'] == book_id:
-                BOOKS.pop(i)
-                response_object = {'status': 'success',
-                                   'message': 'Book removed!'}
-                break
-        else:
-            response_object = {'status': 'error',
-                               'message': f'Book ID({book_id}) not found!'}
+    book = db.session.query(Book).filter_by(book_id=book_id).first()
+    if book:
+        if request.method == 'PUT':
+            post_data = request.get_json()
+            book.title = post_data.get('title')
+            book.author = post_data.get('author')
+            book.read = post_data.get('read')
+            db.session.commit()
+            response_object = {'status': 'success',
+                               'message': 'Book updated!'}
+        elif request.method == 'DELETE':
+            db.session.delete(book)
+            db.session.commit()
+            response_object = {'status': 'success',
+                               'message': 'Book removed!'}
+    else:
+        response_object = {'status': 'error',
+                           'message': f'Book ID({book_id}) not found!'}
 
     return jsonify(response_object)
 
